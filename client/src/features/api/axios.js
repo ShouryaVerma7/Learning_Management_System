85ebd13199fed8e270974b746c383c585e132020
ShouryaVerma7 <vermalucky950@gmail.com>
2026-02-02 23:51:11 +0530

    Fix CORS and cross-site cookies

--- client/src/features/api/axios.js
+++ client/src/features/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: "https://learning-management-system-44su.onrender.com",
+const api = axios.create({
+  baseURL: "https://learning-management-system-44su.onrender.com/api/v1",
   withCredentials: true,
 });
 
-export default API;
+export default api;

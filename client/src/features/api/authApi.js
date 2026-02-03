e29f5911e4e905fc8ad40c305c872ad81d694d7e
ShouryaVerma7 <vermalucky950@gmail.com>
2026-02-03 13:02:16 +0530

    Fix cross-domain cookie auth

--- client/src/features/api/authApi.js
+++ client/src/features/api/authApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { userLoggedIn, userLoggedOut } from "../authSlice"; // IMPORT userLoggedOut
 
-const USER_API = "https://learning-management-system-44su.onrender.com/api/v1/user";
+const USER_API = "import.meta.env.VITE_BACKEND_URL + "/api/v1/user/"";
 
 export const authApi = createApi({
   reducerPath: "authApi",
